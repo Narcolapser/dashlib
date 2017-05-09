d5dc643764c5bcdfbe207c24b5568d7285fb5f79
Toben Archer <sandslash@gmail.com>
2017-05-09 01:46:13 +0000

    Simplified the server a bit.
    I removed the options so that you wouldn't have to specifiy anything at the
    command line. I'll add a config file later, but for now this will help with
    the creation of a simple service.

--- simple-dhcp.py
+++ simple-dhcp.py
@@ -46,91 +46,26 @@ def doTheThing(mac):
 		print "had a woopsie!"
 
 if __name__ == "__main__":
-	parser = OptionParser(description='%prog - a simple DHCP server', usage='%prog [options]')
-	parser.add_option("-a", "--address", dest="address", action="store", help='server ip address (required).')
-	parser.add_option("-i", "--interface", dest="interface", action="store", help='network interface to use (default all interfaces).')
-	parser.add_option("-p", "--port", dest="port", action="store", help='server port to bind (default 67).')
-	parser.add_option("-f", "--from", dest="offerfrom", action="store", help='ip pool from (default x.x.x.100).')
-	parser.add_option("-t", "--to", dest="offerto", action="store", help='ip pool to (default x.x.x.150).')
-	parser.add_option("-b", "--broadcast", dest="broadcast", action="store", help='broadcast ip to reply (x.x.x.254).')
-	parser.add_option("-n", "--netmask", dest="netmask", action="store", help='netmask (default 255.255.255.0).')
-	parser.add_option("-s", "--tftp", dest="tftp", action="store", help='tftp ip address (default ip address provided).')
-	parser.add_option("-d", "--dns", dest="dns", action="store", help='dns ip address (default 8.8.8.8).')
-	parser.add_option("-g", "--gateway", dest="gateway", action="store", help='gateway ip address (default ip address provided).')
-	parser.add_option("-x", "--pxefilename", dest="pxefilename", action="store", help='pxe filename (default pxelinux.0).')
-	parser.add_option("-q", "--quiet", dest="quiet", action="store_true", default=False, help='quiet mode (default off).')
 
-	(options, args) = parser.parse_args()
 
-	if not (args or options.address):
-		parser.print_help()
-		exit(1)
-
-	if options.interface:
-		interface = options.interface
-	else:
-		interface = '' # Symbolic name meaning all available interfaces
-
-	if options.port:
-		port = options.port
-	else:
-		port = '67'
-		port = int(port)
-
-	if options.address:
-		address = options.address
-		elements_in_address = address.split('.')
-		if len(elements_in_address) != 4:
-			sys.exit(os.path.basename(__file__) + ": invalid ip address")
-	else:
-		exit(1)
-
-	if options.offerfrom:
-		offerfrom = options.offerfrom
-	else:
-		offerfrom = '.'.join(elements_in_address[0:3])
-		offerfrom = offerfrom + '.100'
-
-	if options.offerto:
-		offerto = options.offerto
-	else:
-		offerto = '.'.join(elements_in_address[0:3])
-		offerto = offerto + '.150'
-
-	if options.broadcast:
-		broadcast = options.broadcast
-	else:
-		broadcast = '.'.join(elements_in_address[0:3])
-		broadcast = broadcast + '.254'
-
-	if options.netmask:
-		netmask = options.netmask
-	else:
-		netmask = '255.255.255.0'
-
-	if options.tftp:
-		tftp = options.tftp
-	else:
-		tftp = address
-
-	if options.dns:
-		dns = options.dns
-	else:
-		dns = '8.8.8.8'
-
-	if options.gateway:
-		gateway = options.gateway
-	else:
-		gateway = address
-
-	if options.pxefilename:
-		pxefilename = options.pxefilename
-	else:
-		pxefilename = 'pxelinux.0'
-
+	interface = 'wlan0'
+	address = '192.168.1.1'
+	elements_in_address = address.split('.')
+        port = 67
+        offerfrom = '.'.join(elements_in_address[0:3])
+        offerfrom = offerfrom + '.100'
+        offerto = '.'.join(elements_in_address[0:3])
+        offerto = offerto + '.150'
+        broadcast = '.'.join(elements_in_address[0:3])
+        broadcast = broadcast + '.254'
+        netmask = '255.255.255.0'
+        tftp = address
+        dns = '8.8.8.8'
+        gateway = address
+        pxefilename = 'pxelinux.0'
 	leasetime=86400 #int
-
 	leases=[]
+
 	#next line creates the (blank) leases table. This probably isn't necessary.
 
 	if port < 1024:
@@ -147,21 +82,20 @@ if __name__ == "__main__":
 	s.bind(('',port))
 	#s.sendto(data,(ip,port))
 
-	if not options.quiet:
-		print  "Starting %s..." % (os.path.basename(sys.argv[0]))
-		if not address or address == '0.0.0.0':
-		  print "  address: all interfaces"
-		else:
-		  print "  address:	  " + address
-		print   "  tftp:		 " + str(tftp)
-		print   "  gateway:	  " + str(gateway)
-		print   "  dns:		  " + str(dns)
-		print   "  netmask:	  " + str(netmask)
-		print   "  port:		 " + str(port)
-		print   "  pxe filename: " + str(pxefilename)
-		print   "  pid:		  " + str(os.getpid())
-		print   "  serving:	  " + str(offerfrom) + " - " + str(offerto)
-		print   "Press <Ctrl-C> to exit.\n"
+	print  "Starting %s..." % (os.path.basename(sys.argv[0]))
+	if not address or address == '0.0.0.0':
+	  print "  address: all interfaces"
+	else:
+	  print "  address:	  " + address
+	print   "  tftp:		 " + str(tftp)
+	print   "  gateway:	  " + str(gateway)
+	print   "  dns:		  " + str(dns)
+	print   "  netmask:	  " + str(netmask)
+	print   "  port:		 " + str(port)
+	print   "  pxe filename: " + str(pxefilename)
+	print   "  pid:		  " + str(os.getpid())
+	print   "  serving:	  " + str(offerfrom) + " - " + str(offerto)
+	print   "Press <Ctrl-C> to exit.\n"
 
 	while 1: #main loop
 		try:
@@ -180,11 +114,5 @@ if __name__ == "__main__":
 			mac += mac_temp[10:]
 			doTheThing(mac)
 
-#			data=reqparse(message) #handle request
-#			if data:
-#				s.sendto(data,('<broadcast>',68)) #reply
-#			release() #update releases table
 		except KeyboardInterrupt:
 			exit()
-	#	except:
-	#		continue
