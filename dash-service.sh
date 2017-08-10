12e0fd6567479d1312579d2eda0024495d8fbd0f
Toben Archer <sandslash@gmail.com>
2017-08-10 19:54:34 +0000

    Services are confusing.
    I've not yet achieved making this a functional service. It's a
    step that continues to alude me. I think I'm going to have to
    devote a decent amount of time to the study of how linux services
    are made and then I might be able to get this working the way I
    want it to.
    
    The good thing is that the functional side of it works just fine.

--- dash-service.sh
+++ dash-service.sh
@@ -18,11 +18,26 @@
 
 # Defaults
 # SCRIPTNAME=/usr/local/DashServer/DashServer.py
-SCRIPTNAME=/home/toben/Code/dash-server/dash-server.py
+# SCRIPTNAME=/home/toben/Code/dash-server/dash-server.py
+SCRIPTNAME=/usr/local/bin/dash-server
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+DESC="Dash button service"
+NAME=dash-server
+DAEMON=/home/toben/Code/dash-server/$NAME
+DAEMON_ARGS=""
+PIDFILE=/var/run/$NAME.pid
+#SCRIPTNAME=/etc/init.d/$NAME
+
+. /lib/lsb/init-functions
 
 case "$1" in
 start)
         $SCRIPTNAME start
+#	start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PIDFILE --exec $DAEMON --test > /dev/null \
+#		|| return 1
+#	start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- \
+#		$DAEMON_ARGS \
+#		|| return 2
         ;;
 stop)
         $SCRIPTNAME stop
@@ -31,7 +46,8 @@ restart)
         $SCRIPTNAME restart
         ;;
 status)
-        $SCRIPTNAME status
+#        $SCRIPTNAME status
+	status_of_proc "$DAEMON" "$NAME" && exit 0 || exit $?
         ;;
 *)
         echo "Usage: $0 <start|stop|restart|status>" >&2
